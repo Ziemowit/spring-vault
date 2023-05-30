1a835d65e13343200a4b56131adde35bbc4f1da3
Ziemowit Stolarczyk <ziemowits@wayfarerpoints.com>
2023-05-30 09:04:14 +0200

    Create docker compose file to set up the vault

--- src/main/java/pl/ziemowit/spring/vault/VaultPersonProperties.java
+++ src/main/java/pl/ziemowit/spring/vault/VaultPersonProperties.java
@@ -1,11 +1,11 @@
 package pl.ziemowit.spring.vault;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
-@Valid
+@Validated
 @ConfigurationProperties("person")
 public record VaultPersonProperties(@NotBlank String firstName,
                                     @NotBlank String lastName,
